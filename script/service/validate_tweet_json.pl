90a3289f52cadcc31ca61f7d5e7f0091ba97db94
viage <ibn1029@gmail.com>
2013-08-10 16:36:45 +0900

    update

--- script/service/validate_tweet_json.pl
+++ script/service/validate_tweet_json.pl
@@ -64,7 +64,7 @@ sub validate {
     }
     if (@ng_list) {
         warn 'deleteing bloken json of tweet.\n';
-        warn @ng_list."\n";
+        warn  join ', ', @ng_list;
         #my $sth = $self->{dbh}->prepare(qq/delete from tweet where id in (?)/);
         #$sth->execute(join ',', @ng_list);
         #$sth->finish;
