3160fe022cd6d5e95b793b054119e1573c8d267c
ibn1029 <ibn1029@gmail.com>
2013-08-26 12:50:03 +0900

    update

--- lib/BlockHash/Controller/Top.pm
+++ lib/BlockHash/Controller/Top.pm
@@ -19,19 +19,4 @@ sub top {
     );
 }
 
-sub top3 {
-    my $self = shift;
-    my ($total_tweets, $hashtags, $last_updated, $tag_cloud, $weekly_moment_job, $weekly_moment) 
-        = BlockHash::Model::Tweet->get_status;
-    $self->render(
-        is_toppage => 1,
-        total_tweets => $total_tweets,
-        hashtags => $hashtags,
-        last_updated => $last_updated,
-        tag_cloud => $tag_cloud,
-        #weekly_moment_job => $weekly_moment_job,
-        #weekly_moment => $weekly_moment,
-    );
-}
-
 1;
